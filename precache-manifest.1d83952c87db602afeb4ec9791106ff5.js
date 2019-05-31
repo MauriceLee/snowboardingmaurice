2cb3cd1ed6e8874c22876876d764f504d02b446d
Maurice Lee <ee.maurice.lee@gmail.com>
2019-05-31 11:19:30 +0800

    Updates

---
+++ precache-manifest.1d83952c87db602afeb4ec9791106ff5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2267edb85848ee55d2764c373dda05a",
+    "url": "/snowboardingmaurice/index.html"
+  },
+  {
+    "revision": "2b1e8b5ddf7840946cb3",
+    "url": "/snowboardingmaurice/static/css/main.8c5dc37a.chunk.css"
+  },
+  {
+    "revision": "a40f60485db5a8cc77e3",
+    "url": "/snowboardingmaurice/static/js/2.13c2fe96.chunk.js"
+  },
+  {
+    "revision": "2b1e8b5ddf7840946cb3",
+    "url": "/snowboardingmaurice/static/js/main.bc164ab5.chunk.js"
+  },
+  {
+    "revision": "dd47086f34981ca86a2d",
+    "url": "/snowboardingmaurice/static/js/runtime~main.8af5562d.js"
+  },
+  {
+    "revision": "9895bfa4410f4cdb1e05f498b99ad3a6",
+    "url": "/snowboardingmaurice/static/media/img1.9895bfa4.jpg"
+  },
+  {
+    "revision": "66c81c45ffcb60aa9ff19d3417ef49ca",
+    "url": "/snowboardingmaurice/static/media/img2.66c81c45.jpg"
+  },
+  {
+    "revision": "157a281a03b09182bbe41be5fbc3320c",
+    "url": "/snowboardingmaurice/static/media/img3.157a281a.jpg"
+  }
+]);
